0ecaae5b32672736399979744af9a30b4d113f27
Michelle <laimich3@gmail.com>
2017-03-18 15:34:34 -0700

    Updated simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,31 +3,13 @@ public class Simulation {
 		
 	}
 	public static void main(String[] args) {
-		
+
 		//number of customers per seller per hour
 		int N = 10; //change to command line argument later?
 		
-		
-		//keep track of the hour
-		
-		
-		//lock
-		
-		
-		
-		
-		//create 10x10 seating and label with seat numbers
-		Seat[][] seating = new Seat[10][10]; 
-		int numSeat = 1;
-		for (int row = 0; row < 10; row++)
-		{
-			for (int column = 0; column < 10; column++)
-			{
-				seating[row][column] = new Seat(numSeat);
-				numSeat++;
-			}
-		}
-		
+		int maxRows = 10;
+		int maxCols = 10;
+		Seat[][] seating = createSeating(maxRows, maxCols);
 		
 		//create 10 threads representing 10 sellers
 		Seller[] allSellers = new Seller[10];
@@ -38,32 +20,19 @@ public class Simulation {
 			else if (numSeller >= 4 && numSeller < 10) allSellers[numSeller] = new SellerL(seating);
 		}
 		
-		
 		//add N customers for each seller for each hour
-  
 		//initially add N customers for each seller's queue
-		for (int numSeller = 0; numSeller < 10; numSeller++)
-		{
-			for (int count = 0; count < N; count++)
-			{
-				Customer c = new Customer(numSeller);
-				allSellers[numSeller].addCustomer(c);
-			}
-			allSellers[numSeller].sortQueue();
-		}
+		allSellers = addNewCustomers(allSellers, N);
 		
-		//wake up all seller threads, so they can run in "parallel"
-		//use notifyAll for broadcast
-		for (int numSeller = 0; numSeller < allSellers.length; numSeller++)
-		{
-<<<<<<< HEAD
-			new Thread(allSellers[numSeller]).start();
-=======
-			allSellers[numSeller].run();
-			
->>>>>>> 00231be48498f770b6f7fe9ec6fb40e20c15dc17
-		}
 		
+		
+//		//wake up all seller threads, so they can run in "parallel"
+//		allSellers.notifyAll(); //use notifyAll for broadcast
+//		for(int numSellers = 0; numSellers < 10; numSellers++)
+//		{
+//			Thread currentThread = new Thread(allSellers[numSellers]);
+//			currentThread.start();
+//		}
 		
 		
 		//print the following with the current time
@@ -74,10 +43,51 @@ public class Simulation {
 		
 		
 		
+		
 		//seller needs to take in parameters or have some ref to
 		//2D array, time?, universal lock
 	}
 	
+	public static Seat[][] createSeating(int maxRows, int maxCols)
+	{
+		//create 10x10 seating and label with seat numbers
+		Seat[][] seating = new Seat[maxRows][maxCols]; 
+		int numSeat = 1;
+		for (int row = 0; row < maxRows; row++)
+		{
+			for (int column = 0; column < maxCols; column++)
+			{
+				seating[row][column] = new Seat(numSeat);
+				numSeat++;
+			}
+		}
+		return seating;
+	}
 	
+	public static Seller[] addNewCustomers(Seller[] allSellers, int numAdd)
+	{
+		for (int numSeller = 0; numSeller < allSellers.length; numSeller++)
+		{
+			for (int count = 0; count < numAdd; count++)
+			{
+				Customer c = new Customer(numSeller);
+				allSellers[numSeller].addCustomer(c);
+			}
+			allSellers[numSeller].sortQueue();
+		}
+		return allSellers;
+	}
+	
+	public static void printSeating(Seat[][] seating, int maxRows, int maxCols)
+	{
+		for (int row = 0; row < maxRows; row++)
+		{
+			for (int col = 0; col < maxCols; col++)
+			{
+				//if (seating[row][col])
+				System.out.printf("%5d ", seating[row][col]);
+			}
+		}
+	}
 	
 }
