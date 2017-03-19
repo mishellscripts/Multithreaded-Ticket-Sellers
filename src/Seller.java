a5d198e4af24a4538996c359ceb07cde0ff1f64e
Michelle <laimich3@gmail.com>
2017-03-18 17:10:40 -0700

    Made Seller abstract

--- src/Seller.java
+++ src/Seller.java
@@ -35,49 +35,8 @@ public abstract class Seller implements Runnable {
 			customers.add(c);
 	}
 
-<<<<<<< HEAD
 	// seller thread to serve one time slice (1 minute)
-	public void sell() throws InterruptedException {
-		while (!customers.isEmpty()) {						
-			//Object lock = new Object();
-
-			while (customers.isEmpty()) return;
-			// Get customer in queue that is ready
-			Customer customer = customers.peek();
-
-			// Find seat for the customer
-			// Case for Seller H
-			boolean found = false;
-			Seat seat = null;
-
-			synchronized(lock) {
-				find_seat:
-					for (int i = 0; i < seating.length; i++) {
-						for (int j = 0; j < seating[0].length; j++) {
-							if (seating[i][j].isSeatEmpty()) {
-								// Assign seat to customer
-								// Seat number = (Row x 10) + (Col + 1)
-								int seatNum = (i*10)+j+1;
-								seat = new Seat(seatNum);
-								seat.assignSeat(customer);
-								seating[i][j] = seat;
-								break find_seat;
-							}
-						}
-					}
-			lock.notifyAll();
-			}
-
-			if (seat == null) System.out.println("Sorry, the concert is sold out!");
-			else System.out.println("Success! Your seat is " + seat.getSeatNumber());
-
-			customers.remove();
-
-		}
-	}
-=======
 	public abstract void sell();
->>>>>>> stash
 
 	@Override
 	public void run() {
--- src/SellerH.java
+++ src/SellerH.java
@@ -9,45 +9,6 @@ public class SellerH extends Seller {
 		lock = lk;
 	}
 
-<<<<<<< HEAD
-//	public void sell() throws InterruptedException {
-//		while (!customers.isEmpty()) {						
-//			//Object lock = new Object();
-//
-//			while (customers.isEmpty()) return;
-//			// Get customer in queue that is ready
-//			Customer customer = customers.peek();
-//
-//			// Find seat for the customer
-//			// Case for Seller H
-//			boolean found = false;
-//
-//			synchronized(lock) {	
-//				find_seat:
-//					for (int i = 0; i < seating.length; i++) {
-//						for (int j = 0; j < seating[0].length; j++) {
-//							if (seating[i][j].isSeatEmpty()) {
-//								// Assign seat to customer
-//								// Seat number = (Row x 10) + (Col + 1)
-//								int seatNum = (i*10)+j+1;
-//								Seat seat = new Seat(seatNum);
-//								seat.assignSeat(customer);
-//								seating[i][j] = seat;
-//								found = true;
-//								break find_seat;
-//							}
-//						}
-//					}
-//			lock.notifyAll();
-//			}
-//
-//			if (!found) System.out.println("H - Sorry, the concert is sold out!");
-//
-//
-//			customers.remove();
-//		}
-//	}
-=======
 	public void sell() {
 		while (!customers.isEmpty()) {						
 			//Object lock = new Object();
@@ -85,6 +46,5 @@ public class SellerH extends Seller {
 			customers.remove();
 		}
 	}
->>>>>>> stash
 
 }
